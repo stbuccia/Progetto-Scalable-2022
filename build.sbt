c997bbe694e1a95999ce0e8dd1f394307b9c857c
melastone <melania.ghelli@studio.unibo.it>
2022-04-05 19:44:28 +0200

    Working version of IrisKMeansClustering.scala

--- build.sbt
+++ build.sbt
@@ -1,12 +1,12 @@
-
-name := "Progetto-Scalable-2022"
-
-version := "0.1"
-scalaVersion := "2.13.8"
-
-libraryDependencies ++= Seq (
-  "org.apache.spark" %% "spark-core" % "3.2.1",
-  "org.apache.spark" %% "spark-mlib" % "1.6.2"
-)
-
-resolvers += "Maven" at "https://mvnrepository.com/artifact/org.apache.spark"
+import sbt.project
+lazy val root = (project in file("."))
+  .settings(
+    name := "Progetto-Scalable-2022",
+    version := "Progetto-Scalable-2022",
+    scalaVersion := "2.13.8",
+    libraryDependencies ++= Seq(
+      "org.apache.spark" %% "spark-core" % "3.2.1",
+      "org.apache.spark" %% "spark-sql" % "3.2.1",
+      "org.apache.spark" %% "spark-mllib" % "3.2.1",
+    ),
+  )
--- src/main/scala/IrisKMeansClustering.scala
+++ src/main/scala/IrisKMeansClustering.scala
@@ -4,6 +4,17 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.io.Source
 
+// @TODO: learn how we parse data in spark context
+
+/**
+ *
+ * Implements a KMeans clustering model for Iris dataset
+ *
+ * The outputs shows, for each cluster, the center of each feature.
+ * Recall that the features are: sepal lenght, sepal width, petal lenght and petal width
+ *
+ */
+
 object IrisKMeans {
 
   def getResourceFile(filePath: String) = {
@@ -12,6 +23,7 @@ object IrisKMeans {
   }
 
   def main(args: Array[String]): Unit = {
+
     println("Started")
 
     val appName = "IrisKMeans"
@@ -21,8 +33,10 @@ object IrisKMeans {
       .setMaster(master)
     val sc = new SparkContext(conf)
 
+
     println("Loading Iris data...")
-    val dataFilePath = "../resources/iris.data"
+
+    val dataFilePath = "/iris.data.txt"
     val src = getResourceFile(dataFilePath).filter(_.nonEmpty).toList
     val textData = sc.parallelize(src)
     val parsedData = textData
@@ -30,14 +44,28 @@ object IrisKMeans {
       .map(Vectors.dense)
       .cache()
 
+
+    println("Clustering...")
+
+    // Cluster the data into two classes using KMeans
     val numClusters = 3
     val numIterations = 20
-    val clusters = KMeans.train(parsedData, numClusters, numIterations)
+    val clusters = KMeans.train(parsedData, numClusters, numIterations) // returns a KMeansModel obj
 
+
+    println("Results:")
+
+    // Evaluate clustering by computing Within Set Sum of Squared Errors
     val WSSSE = clusters.computeCost(parsedData)
     println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+    // Show significant output
+    clusters.clusterCenters.zipWithIndex.foreach { case (center, idx) =>
+      println(s"Cluster Center ${idx}: ${center}")
+    }
 
     sc.stop()
   }
 
 }
+
