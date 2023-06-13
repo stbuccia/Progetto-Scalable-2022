4a9b0ba8a21152f7345a5a3009e1adb6e4bde93e
melastone <melania.ghelli@studio.unibo.it>
2023-06-13 16:40:37 +0200

    naive apriori implementation started

--- src/main/scala/AssociationRuleLearning/Apriori.scala
+++ src/main/scala/AssociationRuleLearning/Apriori.scala
@@ -3,6 +3,7 @@ package AssociationRuleLearning
 trait Apriori {
   var transactions: Seq[Set[String]]
   var minSupport: Int
+  var minConfidence: Int
 
   def run(): Any
 }
--- src/main/scala/AssociationRuleLearning/AprioriSeq.scala
+++ src/main/scala/AssociationRuleLearning/AprioriSeq.scala
@@ -1,48 +1,66 @@
 package AssociationRuleLearning
 
+import java.io.File
+import scala.io.Source
+import scala.util.control.Breaks
+import scala.util.control.Breaks.{break, breakable}
 
-/* Implementation of Sequential AssociationRuleLearning.Apriori
-  Inspired by wikipedia page and "AssociationRuleLearning.Apriori-Map/Reduce Algorithm", J. Woo paper
+/**
+ * Class for sequantial version of Apriori algorithm
+ * @param inputDataFile file that contains Data - i.e. list of itemset, transactions
+ *
+ * todo: capire perché per usare il trait dobbiamo anche estendere qualcosa...
+ * todo: siccome la lettura dal file viene fatta in modo ridondante sia qua che nel Kmeans, creare una classe di utility col metodo
  */
+class AprioriSeq(inputDataFile: File, support: Int, confidence: Int) extends java.io.Serializable with Apriori{
 
-class AprioriSeq(t: List[Set[String]], m: Int) extends java.io.Serializable with Apriori {
+  // Set up transactions list from Input File
+  override var transactions: Seq[Set[String]] = List()
 
-  var transactions: Seq[Set[String]] = t
-  var minSupport: Int = m
-
-  // joinSort(HashSet(Set(1), Set(2), Set(3), Set(4)), 1)
-  //    => HashSet(Set(3), Set(1), Set(4), Set(2))
-  //
-  // joinSort(HashSet(Set(3), Set(1), Set(4), Set(2)), 2) 
-  //    => HashSet(Set(4, 2), Set(1, 3), Set(4, 3), Set(2, 3), Set(1, 4), Set(1, 2))
-  //
-  // joinSort(HashSet(Set(4, 2), Set(4, 3), Set(2, 3), Set(1, 4), Set(1, 2), 3) 
-  //    => HashSet(Set(1, 2, 4), Set(1, 2, 3), Set(1, 4, 3), Set(4, 3, 2))
-  //
-  // joinSort(HashSet(Set(1, 2, 4), Set(4, 3, 2), 4) 
-  //    => HashSet(Set(4, 3, 2, 1))
-
-  private def joinSort(setL: Set[Set[String]], k: Int) = {
-
-    setL.map(itemset1 => setL.map(itemset2 => itemset1 | itemset2)) //In itemset2 there is also empty set
-      .reduce(_ | _) // Convert set of set in a single set where equal are taken once
-      .filter(_.size == k) //remove results of unions with empty set
+  var itemSet1 : Set[String] = Set()   // creating 1 dimensional itemset
+  val src: List[String] = Source.fromFile(inputDataFile).getLines().filter(_.nonEmpty).drop(1).toList
+  for (line<-src) {
+    val lineSet = line.trim.split(',').toSet
+    if (lineSet.nonEmpty) {
+      transactions = transactions :+ lineSet
+      itemSet1 = itemSet1 ++ lineSet  // it won't have duplicates because of Scala Set class implementation
+    }
   }
 
-  private def prune(setC: Set[Set[String]]): Set[Set[String]] = {
-    setC.filter(set => transactions.count(set.subsetOf(_)) >= minSupport)
-  }
-  
-  def apriori_sequential(setL: Set[Set[String]], k: Int): Set[Set[String]] = {
-    val frequentElements = prune(joinSort(setL, k))
-    if (frequentElements.isEmpty)
-      setL
-    else
-      apriori_sequential(frequentElements, k + 1)
+  // Set minimum support and minimum confidence
+  override var minSupport: Int = support
+  override var minConfidence: Int = confidence
+
+
+  /**
+   * Prunes a given Candidates Set by checking if its subsets satisfy the minimum support.
+   * @param candidatesSet
+   * @return
+   */
+//  private def prune(candidatesSet: Set[Set[String]]): Set[Set[String]] = {
+//
+//  }
+
+  def run(): Unit = {
+    var k = 2
+    var currentLSet = itemSet1
+    var candidatesSet = currentLSet.subsets().filter(_.size == k).toSet
+    println(candidatesSet)
+//    breakable {
+//      while (true) {
+//        var candidatesSet = currentLSet.subsets().filter(_.size == k).toSet
+//        println(candidatesSet)
+//        val currentItemCombs : Set[(Set[String], Double)] = currentCSet.map( wordSet => (wordSet, getSupport(wordSet)))
+//                                          .filter( wordSetSupportPair => (wordSetSupportPair._2 > minSupport))
+//        val currentLSet = currentItemCombs.map( wordSetSupportPair => wordSetSupportPair._1).toSet
+//        if (currentLSet.isEmpty) break
+//        currentCSet = currentLSet.map( wordSet => currentLSet.map(wordSet1 => wordSet | wordSet1))
+//                                                            .reduceRight( (set1, set2) => set1 | set2)
+//                                                            .filter( wordSet => (wordSet.size==k))
+//        itemCombs = itemCombs | currentItemCombs
+//        k += 1
+//      }
+//  }
   }
 
-  def run() = {
-    val setC = transactions.reduce(_ | _).map(Set(_))
-    apriori_sequential(setC, 1)
-  }
 }
