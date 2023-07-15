f4e9ec6f27528b6bb605bdbcfe032db91d06aa63
melastone <melania.ghelli@studio.unibo.it>
2023-07-15 16:55:40 +0200

    updated Apriori interface

--- src/main/scala/associationrulelearning/Apriori.scala
+++ src/main/scala/associationrulelearning/Apriori.scala
@@ -4,11 +4,10 @@ import org.apache.spark.rdd.RDD
 trait Apriori {
 
   var itemSet : Set[String] = Set("SH", "NH", "Q1", "Q2", "Q3", "Q4", "LOW_MAG", "MED_MAG", "HIGH_MAG", "LOW_DEPTH", "MED_DEPTH", "HIGH_DEPTH")
+
   var transactions: RDD[Set[String]]
   var minSupport: Int
   var minConfidence: Double
-
-  //var generatedItemsets: Map[Set[String], Int]
 
   //var frequentItemsets: Set[Set[String]]
   //var associationRules : List[(Set[String], Set[String], Double)]
