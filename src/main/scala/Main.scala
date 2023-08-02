ca5d162d43ca5ecf6abe650578d9543706f2a407
carrozzogianluigi <gianluigi.carrozzo@studio.unibo.it>
2023-08-02 19:50:06 +0200

    fix rules duplicate

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -85,7 +85,11 @@ object Main {
     algorithms match {
       case alg :: tail => {
         val (rules, time_elapsed) = time(alg, runAprioriForEachCluster(sc, numClusters, normalizedData, alg))
-        val res = rules.reduceLeft((a,b) => a.union(b)).distinct()
+        val res = rules.reduceLeft((a,b) => a.union(b))
+          .map(rule => ((rule._1, rule._2), rule._3))
+          .groupByKey()
+          .map(rule => (rule._1._1, rule._1._2, rule._2.head ) )
+
         writeAssociationRulesToCSV(sparkSession, verify(res, normalizedData), outputFolder + "/associationrules/" + alg)
         (alg, time_elapsed) :: executeAlgorithms(tail, sparkSession, sc, numClusters, normalizedData, outputFolder)
       }
