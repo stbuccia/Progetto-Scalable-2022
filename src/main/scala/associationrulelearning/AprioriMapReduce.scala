6786afc6b386d08362a991940d09e14a1452d70d
carrozzogianluigi <gianluigi.carrozzo@studio.unibo.it>
2023-08-04 00:02:45 +0200

    risolto problema generazione association rules AprioriMapReduce

--- src/main/scala/associationrulelearning/AprioriMapReduce.scala
+++ src/main/scala/associationrulelearning/AprioriMapReduce.scala
@@ -54,7 +54,6 @@ class AprioriMapReduce(dataset: RDD[Set[String]]) extends Serializable with Apri
                               rdd_size: Double,
                               min_support: Double,
                               i: Int) = {
-    //println("\nGenerate itemsets with size " + i + "...")
 
     //TODO collect() dopo countItemsetsSizeN?
     val array_itemsetsSizeN = rdd_itemsets
@@ -62,7 +61,6 @@ class AprioriMapReduce(dataset: RDD[Set[String]]) extends Serializable with Apri
       .flatMap(x => generateItemsetsSizeN(x, labelSet))
       .collect().toSet
     val output = countItemsetsSizeN(rdd, array_itemsetsSizeN, rdd_size, min_support)
-
 
     output
   }
@@ -93,44 +91,16 @@ class AprioriMapReduce(dataset: RDD[Set[String]]) extends Serializable with Apri
       .filter(x => (x._2.toDouble / rdd_size) >= min_sup)
   }
 
-/*
-  def generateAssociationRulesFromSubset(rdd: RDD[Set[String]],
-                                         itemset: Set[String],
-                                         itemset_support: Int,
-                                         subItemset: Set[String],
-                                         confidence: Double) = {
-    val rules = rdd
-      .filter(x => subItemset.subsetOf(x))
-      .map(x => (subItemset, 1))
-      .reduceByKey((x, y) => x + y)
-      .map(x => (x._1, itemset -- subItemset, itemset_support.toDouble / x._2.toDouble))
-      .filter(x => x._3 >= confidence)
 
-    //TODO è necessaria la reverseRules?
-    /*
-        val reverseRules = rdd
-          .filter(x => (itemset -- subItemset).subsetOf(x))
-          .map(x => ((itemset -- subItemset), 1))
-          .reduceByKey((x, y) => x + y)
-          .map(x => (x._1, itemset -- (itemset -- subItemset), itemset_support / x._2.toDouble))
-          .filter(x => x._3 >= confidence)
-
-        rules ++ reverseRules
-
-     */
-    rules
-  }
-*/
 
   def generateAssociationRules(rdd_itemsets: RDD[(Set[String], Int)],
                                confidence: Double,
                                rdd: RDD[Set[String]]) = {
 
     val array_subsets = rdd_itemsets
-      .flatMap(itemset => {
-        val rangeIncl = Range.inclusive(1, itemset._1.toArray.length - 1)
-        rangeIncl.flatMap(y => itemset._1.subsets(y).toSet)
-      }).collect()
+      .flatMap(itemset => itemset._1.subsets().toSet.filter(_.nonEmpty) )
+      .distinct()
+      .collect()
 
     val array_subsets_pair = rdd
       .flatMap(rdd_row => {
@@ -147,9 +117,6 @@ class AprioriMapReduce(dataset: RDD[Set[String]]) extends Serializable with Apri
           .filter(subset_pair => subset_pair._1.subsetOf(itemset._1) && subset_pair._1.size < itemset._1.size)
           .map(subset_pair => (subset_pair._1, itemset._1 -- subset_pair._1, itemset._2.toDouble / subset_pair._2.toDouble))
           .filter(rule => rule._3 >= confidence)
-          .sortBy(_._3)
-        //.sortBy(_._3, ascending = false)
-        //TODO sort
       })
 
     associationRules
