45fe47cfc08221b387cfc18d4e96b276ae760be5
carrozzogianluigi <gianluigi.carrozzo@studio.unibo.it>
2023-08-13 19:44:17 +0200

    aggiunta cartella con dataset finali

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -69,7 +69,7 @@ object Main {
 
     // Normalize data
 
-    val normalizedData: RDD[(Int, Set[String])] = clusteredData.map(entry => (entry._1, labelConversion(entry._2)))
+    val normalizedData: RDD[(Int, Set[String])] = clusteredData.map(entry => (entry._1, labelConversion(entry._2)))//.persist()
 
     val algorithms = if (simulation) List("aprioriseq", "apriorispc", "apriorimapreduce", "fpgrowth") else List(classifier)
 
